f3773128a71fed67f89ce628cc7377b1640cc6fa
Jesús Briceño <jesusbingucv@gmail.com>
2023-09-07 08:14:47 -0400

    [ADD] _load_data method's code

--- detection/lstm_detection/preprocessing.py
+++ detection/lstm_detection/preprocessing.py
@@ -1,12 +1,15 @@
 import os
 import sys
+import glob
 from typing import Any
+from rackio_AI import RackioAI
 
 # This code line is to avoid import relative error
 sys.path.append("..")
 
 from utils import parse_config, convert_list2tuple, report_done
 from feature_extraction import FeatureExtraction
+
 
 class DetectionPreprocessPipeline:
     """This class preprocess the data for training LSTM classification model for detection.
@@ -15,11 +18,30 @@ class DetectionPreprocessPipeline:
         self.configs = parse_config(configs)
 
     @report_done
-    def _load_data(self):
-        pass
+    def _load_data(self) -> list:
+        """Loads the data in pkl format
+        """
+        path = os.path.join(*self.configs['DATA_PATH']['path'])
+        filename = self.configs['DATA_PATH']['filename']
+        self.data = list()
+        files = list()
+        if filename:
+
+            files.extend([os.path.join(path, filename)])
+
+        else:
+
+            path = os.path.join(*self.configs['DATA_PATH']['path']) + os.path.sep + '*.pkl'
+            files = glob.glob(path)
+
+        for file in files:
+
+            self.data.extend(RackioAI.load(file))
+        
+        return self.data
     
     def run(self):
         self._load_data()
-
+        
     def __call__(self) -> Any:
         self.run()
